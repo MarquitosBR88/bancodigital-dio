78f8b6f06b874b4e82fba911b96d1600c713829f
MarquitosBR88 <marcus.vi.mar@gmail.com>
2022-01-21 14:03:35 -0300

    Banco Digital

---
+++ src/dio/projetos/banco/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package dio.projetos.banco;
+
+public class ContaPoupanca extends Conta {
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("*** Extrato Conta Poupança ***");
+        super.ImprimirInfosComuns();
+    }
+
+}
